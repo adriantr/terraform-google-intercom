4198b3abde544acc6eabbf5096731c16b1f6a576
Fredrik Schöld <schold.fredrik@gmail.com>
2023-03-31 09:08:01 +0200

    Add second load balancer for http to https redirect (#2)

--- compute.tf
+++ compute.tf
@@ -22,10 +22,6 @@ resource "google_compute_backend_service" "main" {
 resource "google_compute_url_map" "main" {
   name            = "intercom"
   default_service = google_compute_backend_service.main.id
-  default_url_redirect {
-    https_redirect = true
-    strip_query    = false
-  }
 }
 
 resource "google_compute_target_https_proxy" "main" {
@@ -41,4 +37,25 @@ resource "google_compute_global_forwarding_rule" "main" {
   load_balancing_scheme = "EXTERNAL"
   target                = google_compute_target_https_proxy.main.id
   port_range            = 443
+}
+
+resource "google_compute_url_map" "http-redirect" {
+  name = "http-redirect"
+
+  default_url_redirect {
+    strip_query            = false
+    https_redirect         = true
+  }
+}
+
+resource "google_compute_target_http_proxy" "http-redirect" {
+  name    = "http-redirect"
+  url_map = google_compute_url_map.http-redirect.self_link
+}
+
+resource "google_compute_global_forwarding_rule" "http-redirect" {
+  name       = "http-redirect"
+  target     = google_compute_target_http_proxy.http-redirect.self_link
+  ip_address = google_compute_global_address.main.address
+  port_range = "80"
 }
